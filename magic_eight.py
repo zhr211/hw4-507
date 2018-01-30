f6be47bf24bddd80dd89efb28fe56ef35bbf6bdf
jingwen <jingwenc@umich.edu>
2018-01-29 20:35:38 -0500

    already changed

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,11 @@
-def answer():
-    ans=input('what is your question?')
-    return ans
+from random import randrange
+#def ask_que():
+ans = input("What is your question?")
+ans_list = ["It is certain","It is decidedly so","Without a doubt",
+"Yes definitely","You may rely on it","As I see it, yes","Most likely",
+"Outlook good","Yes","Signs point to yes","Reply hazy try again",
+"Ask again later","Better not tell you now","Cannot predict now",
+"Concentrate and ask again","Don't count on it","My reply is no",
+"My sources say no","Outlook not so good","Very doubtful"]
+magic_ans = ans_list[randrange(len(ans_list))]
+print(magic_ans)
